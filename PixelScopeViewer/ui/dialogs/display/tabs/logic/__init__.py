c1842297a4336c57b1a30054cc98d176b60a3cec
orcasemi <gatyagatya06@gmail.com>
2025-10-28 00:53:09 +0900

    brightness_tab.pyのコードを短縮

--- PixelScopeViewer/ui/dialogs/display/tabs/logic/__init__.py
+++ PixelScopeViewer/ui/dialogs/display/tabs/logic/__init__.py
@@ -2,16 +2,24 @@
 
 from .brightness_utils import (
     determine_dtype_defaults,
+    get_dtype_defaults,
     clamp_value,
     slider_to_value,
     value_to_slider,
+    round_to_power_of_2,
+    format_value_label,
+    format_gain_label,
 )
 from .brightness_builder import BrightnessUIBuilder
 
 __all__ = [
     "determine_dtype_defaults",
+    "get_dtype_defaults",
     "clamp_value",
     "slider_to_value",
     "value_to_slider",
+    "round_to_power_of_2",
+    "format_value_label",
+    "format_gain_label",
     "BrightnessUIBuilder",
 ]

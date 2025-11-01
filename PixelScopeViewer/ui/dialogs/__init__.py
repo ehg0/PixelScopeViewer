76a7756763c80129d44250916ec2ac415243fac5
orcasemi <gatyagatya06@gmail.com>
2025-11-01 17:08:31 +0900

    jsonで画像の情報を追加読み込みできるようにする Fixes #21

--- PixelScopeViewer/ui/dialogs/__init__.py
+++ PixelScopeViewer/ui/dialogs/__init__.py
@@ -4,5 +4,6 @@ from .help_dialog import HelpDialog
 from .diff_dialog import DiffDialog
 from .display import BrightnessDialog
 from .analysis import AnalysisDialog
+from .features_dialog import FeaturesDialog
 
-__all__ = ["HelpDialog", "DiffDialog", "BrightnessDialog", "AnalysisDialog"]
+__all__ = ["HelpDialog", "DiffDialog", "BrightnessDialog", "AnalysisDialog", "FeaturesDialog"]
